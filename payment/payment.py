f5ed053886ef50aa4d7457c54f84e2507ad92cf9
Mch88-ai <mc.huang.2024@computing.smu.edu.sg>
2026-03-24 09:52:26 +0800

    modified:   payment/payment.py

--- payment/payment.py
+++ payment/payment.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (environ.get('dbURL') or 'mysql+mysqlconnector://root:jpPOaVCbCXnTWjDOBzPtDoRKYwqqiClR@caboose.proxy.rlwy.net:45033/payment')
+app.config['SQLALCHEMY_DATABASE_URI'] = (environ.get('dbURL'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 
