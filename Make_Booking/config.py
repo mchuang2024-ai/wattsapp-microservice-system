09f7c49dfd4339c97e3a9ffa50371411d23fa181
Zachary Tay <zachary.tay.2024@computing.smu.edu.sg>
2026-03-24 18:02:05 +0800

    Make Booking composite

---
+++ Make_Booking/config.py
@@ -0,0 +1,15 @@
+import os
+
+# Database logging (optional)
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    'SQLALCHEMY_DATABASE_URI',
+    'mysql+mysqlconnector://root:rootpass@localhost:3306/pulsepark'
+)
+
+# RabbitMQ
+type = 'amqp'
+RABBITMQ_URL = os.environ.get('RABBITMQ_URL', 'amqp://admin:password123@localhost:5672/%2F')
+
+# Downstream service URLs
+PAYMENT_URL = os.environ.get('PAYMENT_URL', 'http://payment:5002/payment/hold')
+BOOKING_URL = os.environ.get('BOOKING_URL', 'http://booking:5003/bookings')
