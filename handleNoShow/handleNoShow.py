ad7609f5b9f3c2b7860fa6bfb4ebf47c11480c30
Mch88-ai <mc.huang.2024@computing.smu.edu.sg>
2026-03-31 21:39:08 +0800

    modified:   handleNoShow/handleNoShow.py

--- handleNoShow/handleNoShow.py
+++ handleNoShow/handleNoShow.py
@@ -207,6 +207,6 @@ def handleNoShow():
 
 # Execute this program if it is run as a main script (not by 'import')
 if __name__ == "__main__":
-    print("This is flask " + os.path.basename(__file__) + " for placing an order...")
+    print("This is flask " + os.path.basename(__file__) + " handling no show..")
     connectAMQP()
     app.run(host="0.0.0.0", port=5100, debug=True)
